aa15c827fa3c5b945d69e0469796f3b7602f513b
krishya <krishya.makkar@student.adelaide.edu.au>
2024-10-11 18:14:33 +1030

    Added comments  for better code readability

--- Waiter.cpp
+++ Waiter.cpp
@@ -1,7 +1,9 @@
 #include "Waiter.h"
 
+// Initializes the Waiter's name and ID using the Employee base class constructor
 Waiter::Waiter(const std::string& name, int id) : Employee(name, id) {}
 
+// Outputs the Waiter's name and indicates that they are serving food to customers
 void Waiter::perform_task() const {
     std::cout << "Waiter " << name << " is serving food to customers.\n";
 }
