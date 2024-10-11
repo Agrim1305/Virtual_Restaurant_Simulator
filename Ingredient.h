230ab554fd4268bbe53c9e693f26f052fe8e03ac
krishya <krishya.makkar@student.adelaide.edu.au>
2024-10-11 15:27:18 +1030

    Added comments  for better code readability

--- Ingredient.h
+++ Ingredient.h
@@ -4,15 +4,19 @@
 #include <string>
 #include <stdexcept>
 
+// Declaration of the Ingredient class
 class Ingredient {
 private:
-    std::string name;
-    int quantity;
+    std::string name; // Name of the ingredient
+    int quantity;     // Quantity of the ingredient available in stock
 
 public:
+ // Constructor initializes name to "Unknown" and quantity to 0
     Ingredient() : name("Unknown"), quantity(0) {}
+ // Constructor to initialize the ingredient with a specific name and quantity
     Ingredient(const std::string& name, int quantity);
 
+// Method to return the name of the ingredient
     std::string get_name() const;
     int get_quantity() const;
 
