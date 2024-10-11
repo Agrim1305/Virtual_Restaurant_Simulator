21a6edfc5cda5345330f971049fd20a8b05f53ca
krishya <krishya.makkar@student.adelaide.edu.au>
2024-10-11 17:25:18 +1030

    Added comments  for better code readability

--- MenuItem.h
+++ MenuItem.h
@@ -5,6 +5,7 @@
 
 class MenuItem {
 private:
+// Private members to store the name, price, preparation time, main ingredient, and quantity of the menu item
     std::string name;
     float price;
     int prep_time;
@@ -12,6 +13,7 @@ private:
     int quantity;  // Add quantity attribute
 
 public:
+ // Constructor to initialize a menu item with its name, price, preparation time, main ingredient, and quantity
     MenuItem(const std::string& name, float price, int prep_time, const std::string& main_ingredient, int quantity);
     
     const std::string& get_name() const;
