bfbea525d0605502d4ef691615195bc4b1d23165
krishya <krishya.makkar@student.adelaide.edu.au>
2024-10-11 17:08:13 +1030

    Added comments  for better code readability

--- Manager.h
+++ Manager.h
@@ -4,10 +4,11 @@
 #include "Employee.h"
 #include <iostream>
 
+// Declaration of the Manager class that inherits from the Employee class
 class Manager : public Employee {
 public:
-    Manager(const std::string& name, int id);
-    void perform_task() const override;
+    Manager(const std::string& name, int id); //Manager class that initializes the Manager's name and ID
+    void perform_task() const override;  // This makes the Manager class provide its own implementation of perform_task
 };
 
 #endif
