bef8b80d690bb41838bfdfcff2b78f6837487eef
krishya <krishya.makkar@student.adelaide.edu.au>
2024-10-11 15:18:45 +1030

    Added comments  for better code readability

--- HeadChef.h
+++ HeadChef.h
@@ -4,9 +4,13 @@
 #include "Chef.h"
 #include <iostream>
 
+// Declaration of the HeadChef class that inherits from the Chef class
 class HeadChef : public Chef {
 public:
+// Constructor for the HeadChef class that initializes the name and ID of the HeadChef
     HeadChef(const std::string& name, int id);
+    
+// Method to define the specific task a HeadChef performs
     void perform_task() const override;
 };
 
