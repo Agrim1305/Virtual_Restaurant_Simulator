24a108ad45c6d7c6035ce593b2e8095b551c9821
krishya <krishya.makkar@student.adelaide.edu.au>
2024-10-11 18:13:01 +1030

    Added comments  for better code readability

--- Table.h
+++ Table.h
@@ -1,12 +1,14 @@
 #ifndef TABLE_H
 #define TABLE_H
 
+// Declaration of the Table clas
 class Table {
 private:
-    int table_number;
-    bool occupied;
+    int table_number; // The unique number assigned to the table
+    bool occupied;    // Indicates whether the table is currently occupied or not
 
 public:
+// Constructor to initialize the table with a specific table number and set its initial state to unoccupied
     Table(int table_number);
 
     int get_table_number() const;
