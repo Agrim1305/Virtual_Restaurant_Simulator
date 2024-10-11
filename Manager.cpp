5c62956f1d1cbd948f21fed7d4f2188218cb647d
krishya <krishya.makkar@student.adelaide.edu.au>
2024-10-11 17:06:38 +1030

    Added comments  for better code readability

--- Manager.cpp
+++ Manager.cpp
@@ -1,7 +1,9 @@
 #include "Manager.h"
 
+// Initializes the Manager's name and ID using the Employee base class constructor
 Manager::Manager(const std::string& name, int id) : Employee(name, id) {}
 
+// Outputs the Manager's name and indicates that they are supervising the restaurant
 void Manager::perform_task() const {
     std::cout << "Manager " << name << " is supervising the restaurant.\n";
 }
