4c4b83e58699a429eb8df0cdaf289ffcb53cd378
krishya <krishya.makkar@student.adelaide.edu.au>
2024-10-11 18:15:47 +1030

    Added comments  for better code readability

--- Waiter.h
+++ Waiter.h
@@ -4,8 +4,10 @@
 #include "Employee.h"
 #include <iostream>
 
+// Declaration of the Waiter class that inherits from the Employee class
 class Waiter : public Employee {
 public:
+// Waiter class that initializes the Waiter's name and ID
     Waiter(const std::string& name, int id);
     void perform_task() const override;
 };
