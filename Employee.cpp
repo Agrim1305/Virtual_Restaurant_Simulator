d5a5bf8375b41db6139a9f9147b2a65dedcafca4
krishya <krishya.makkar@student.adelaide.edu.au>
2024-10-11 15:06:52 +1030

    Added comments  for better code readability

--- Employee.cpp
+++ Employee.cpp
@@ -1,11 +1,14 @@
 #include "Employee.h"
 
+//Employee's name and ID using the provided arguments
 Employee::Employee(const std::string& name, int id) : name(name), id(id) {}
 
+// Method to return the Employee's name
 std::string Employee::get_name() const {
     return name;
 }
 
+// Method to return the Employee's ID
 int Employee::get_id() const {
     return id;
 }
