fb010a7faffb64fa6cec78a09e33e1ff2b6aeec4
krishya <krishya.makkar@student.adelaide.edu.au>
2024-10-11 17:14:21 +1030

    Added comments  for better code readability

--- Menu.cpp
+++ Menu.cpp
@@ -3,14 +3,17 @@
 #include <iostream>
 #include <sstream>   // For stringstream
 
+// Adds a new item to the menu
 void Menu::add_item(const MenuItem& item) {
     items.push_back(item);
 }
 
+// Method to return a constant reference to the list of menu items
 const std::vector<MenuItem>& Menu::get_items() const {
     return items;
 }
 
+//  Displays the menu items along with their details: name, price, and preparation time
 void Menu::display_menu() const {
     std::cout << "Available Menu Items:\n";
     for (size_t i = 0; i < items.size(); ++i) {
@@ -28,6 +31,7 @@ void Menu::save_to_file(const std::string& filename) const {
         return;
     }
 
+// Menu item details are added to the file
     for (const auto& item : items) {
         file << item.get_name() << "," << item.get_price() 
              << "," << item.get_prep_time() << "," 
@@ -41,7 +45,7 @@ void Menu::save_to_file(const std::string& filename) const {
 // Load menu items from a file
 void Menu::load_from_file(const std::string& filename) {
     std::ifstream file(filename);
-    if (!file.is_open()) {
+    if (!file.is_open()) { // Check if the file was successfully opened
         std::cerr << "Error: Could not open file for loading menu.\n";
         return;
     }
@@ -55,18 +59,19 @@ void Menu::load_from_file(const std::string& filename) {
         float price;
         int prep_time, quantity;
 
-        std::getline(ss, name, ',');
-        ss >> price;
-        ss.ignore();
-        ss >> prep_time;
-        ss.ignore();
-        std::getline(ss, ingredient, ',');
-        ss >> quantity;
+        std::getline(ss, name, ','); // Get the name of the menu item
+        ss >> price; // Get the price of the menu item
+        ss.ignore(); // Skips the comma
+        ss >> prep_time; // Get the preparation time of the menu item
+        ss.ignore();  // Skips the comma
+        std::getline(ss, ingredient, ','); // Get the main ingredient of the menu item
+        ss >> quantity; // Get the quantity of the ingredient used
+
 
         // Ensure that the correct number of arguments are passed to the MenuItem constructor
         items.push_back(MenuItem(name, price, prep_time, ingredient, quantity));
     }
 
-    file.close();
+    file.close(); 
     std::cout << "Menu loaded from " << filename << "\n";
 }
