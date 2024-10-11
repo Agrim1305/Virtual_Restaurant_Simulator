155e6794d8fcabedeb601ef5521706ad1b696069
krishya <krishya.makkar@student.adelaide.edu.au>
2024-10-11 18:11:07 +1030

    Added comments  for better code readability

--- Table.cpp
+++ Table.cpp
@@ -1,19 +1,26 @@
 #include "Table.h"
 
+// Table Class
+// Initializes the table with a specified table number and sets its initial state to unoccupied
 Table::Table(int table_number) : table_number(table_number), occupied(false) {}
 
+ // Method to return the table's number
 int Table::get_table_number() const {
     return table_number;
 }
 
+// Checks if the table is currently occupied
+// Return True if occupied, otherwise False
 bool Table::is_occupied() const {
     return occupied;
 }
 
+// Marks the table as occupied
 void Table::occupy() {
     occupied = true;
 }
 
+// Marks the table as vacant
 void Table::vacate() {
     occupied = false;
 }
