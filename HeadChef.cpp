f0bc2ececa9a9ada9a5fc9506ae08e9d1a68c6c7
krishya <krishya.makkar@student.adelaide.edu.au>
2024-10-11 15:16:33 +1030

    Added comments  for better code readability

--- HeadChef.cpp
+++ HeadChef.cpp
@@ -1,7 +1,9 @@
 #include "HeadChef.h"
 
+//HeadChef class, initializes the HeadChef's name and ID
 HeadChef::HeadChef(const std::string& name, int id) : Chef(name, id) {}
 
+// Outputs the HeadChef's name and indicates that they are managing the kitchen
 void HeadChef::perform_task() const {
     std::cout << "Head Chef " << name << " is managing the kitchen.\n";
 }
