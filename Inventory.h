83e20e0f1591fe0bdc769b14ff0e4dd6dc7188bc
krishya <krishya.makkar@student.adelaide.edu.au>
2024-10-11 17:04:49 +1030

    Added comments  for better code readability

--- Inventory.h
+++ Inventory.h
@@ -7,14 +7,23 @@
 #include <iostream>
 #include <fstream>  // For file I/O
 
+// Declaration of the Inventory class to manage the stock of ingredients
 class Inventory {
 private:
+    // Map to store ingredients with their names as keys and Ingredient objects as values
     std::map<std::string, Ingredient> stock;
 
 public:
+    // Method to restock a specified quantity of an ingredient
     void restock(const std::string& ingredient, int quantity);
+
+     // Method to check if a specified ingredient is in stock and available
     bool check_stock(const std::string& ingredient) const;
+
+    // Method to use a specified amount of an ingredient from the stock
     void use_ingredient(const std::string& ingredient, int amount);
+
+    // Method to display the current inventory and the quantity of each ingredient
     void display_inventory() const;
     
     // New file I/O methods
