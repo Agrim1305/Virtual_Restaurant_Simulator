f1a073d51c3c5e9f3c49400509c7d3118d417a18
krishya <krishya.makkar@student.adelaide.edu.au>
2024-10-11 17:57:57 +1030

    Added comments  for better code readability

--- Person.h
+++ Person.h
@@ -3,15 +3,18 @@
 
 #include <string>
 
+// Declaration of the Person class, which serves as a base class for other classes
 class Person {
 protected:
-    std::string name;
-    int id;
+    std::string name; // Name of the person
+    int id; // Unique ID for the person
 
 public:
+// Initialize the Person's name and ID
     Person(const std::string& name, int id);
     virtual ~Person() {}
 
+// Method to return the name and ID 
     std::string get_name() const;
     int get_id() const;
 };
