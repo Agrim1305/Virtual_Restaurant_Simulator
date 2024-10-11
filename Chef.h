afbba51cdebb9bbddc2754a5a20c4cc12664d0df
krishya <krishya.makkar@student.adelaide.edu.au>
2024-10-11 14:57:56 +1030

    Added comments to Chef.h for better code readability

--- Chef.h
+++ Chef.h
@@ -4,9 +4,13 @@
 #include "Employee.h"
 #include <iostream>
 
+// Declaration of the Chef class that inherits from Employee
 class Chef : public Employee {
 public:
+// Constructor that takes the name and ID of the Chef
     Chef(const std::string& name, int id);
+
+     // Overridden method to define the specific task a Chef performs
     void perform_task() const override;
 };
 
