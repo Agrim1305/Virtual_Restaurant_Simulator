6d0db35a0ee41040e3f79b3c4725f42f2de66a46
krishya <krishya.makkar@student.adelaide.edu.au>
2024-10-11 14:56:51 +1030

    Added comments to Chef.cpp for better code readability

--- Chef.cpp
+++ Chef.cpp
@@ -1,7 +1,9 @@
 #include "Chef.h"
 
+//Chef class, initializes the Chef's name and ID
 Chef::Chef(const std::string& name, int id) : Employee(name, id) {}
 
+// Outputs the Chef's name and indicates that the Chef is preparing food
 void Chef::perform_task() const {
     std::cout << "Chef " << name << " is preparing food.\n";
 }
