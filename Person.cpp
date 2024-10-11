f51d3f5302bcb2f62bf5895fbfccd2495229c561
krishya <krishya.makkar@student.adelaide.edu.au>
2024-10-11 17:30:20 +1030

    Added comments  for better code readability

--- Person.cpp
+++ Person.cpp
@@ -1,11 +1,13 @@
 #include "Person.h"
 
+// Initializes the Person's name and ID
 Person::Person(const std::string& name, int id) : name(name), id(id) {}
 
+// Method to return the Person's name
 std::string Person::get_name() const {
     return name;
 }
-
+//  Method to return the Person's ID
 int Person::get_id() const {
     return id;
 }
